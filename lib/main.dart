de23d8ea93d825dbcaa8e500a557657a27f1cb8c
vishnuvarshan <varshan.17mc@kct.ac.in>
2020-07-03 14:46:33 +0530

    Added actions

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String titlename = "Appbar";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('AppBar'),
+          title: Center(
+            child: Text(
+              titlename,
+              style:
+                  TextStyle(color: Colors.yellow, fontWeight: FontWeight.bold),
+            ),
+          ),
+          backgroundColor: Colors.purple,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {
+                setState(() {
+                  titlename = "Home";
+                });
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.notifications),
+              onPressed: () {
+                setState(() {
+                  titlename = "Notification";
+                });
+              },
+            ),
+          ],
         ),
       ),
     );
